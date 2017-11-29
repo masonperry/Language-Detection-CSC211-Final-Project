b30a44d70bd715b291c855056062af89cd9daaf7
Mason <mason_perry@my.uri.edu>
2017-11-29 14:53:19 -0500

    Editied freq and added compile

--- frequencies.cpp
+++ frequencies.cpp
@@ -4,16 +4,25 @@
 #include <vector>
 #include <fstream>
 
+
+std::string trans(std::string seq) {
+  std::string result;
+	for (int i = 0; i < (int) seq.length() - 2; i+=3) {
+		std::string tri = seq.substr(i,3);
+
+
+
 std::vector<std::string> newSeq;
 for(unsigned long i = 0; i<= seq.length(); i ++){
-             //Makes trigram the size of 3
-             tri += seq[i];
-             tri += seq[i+1];
-             tri += seq[i+2];
-             if (Condon.length() < 3){
-                    //returns Amino if the length of Condon is shorter than three
-                    return newSeq;
-                    break;
-             }
-             //Find the Amino Acid
+  //Makes trigram the size of 3
+  tri += seq[i];
+  tri += seq[i+1];
+  tri += seq[i+2];
+  std::cout<< tri << endl;
+  if (tri.length() < 3){
+    //returns new sequence if the length of trigram is shorter than three
+    return newSeq;
+    break;
+    }
+      //Find the new Sequence
              else{
