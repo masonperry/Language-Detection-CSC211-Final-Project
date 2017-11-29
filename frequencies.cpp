1c022526b28d1d5001dde0939432a15ab77a7d9d
Mason <mason_perry@my.uri.edu>
2017-11-29 14:39:44 -0500

    added to frequency

--- frequencies.cpp
+++ frequencies.cpp
@@ -3,3 +3,17 @@
 #include <cstdlib>
 #include <vector>
 #include <fstream>
+
+std::vector<std::string> newSeq;
+for(unsigned long i = 0; i<= seq.length(); i ++){
+             //Makes trigram the size of 3
+             tri += seq[i];
+             tri += seq[i+1];
+             tri += seq[i+2];
+             if (Condon.length() < 3){
+                    //returns Amino if the length of Condon is shorter than three
+                    return newSeq;
+                    break;
+             }
+             //Find the Amino Acid
+             else{
