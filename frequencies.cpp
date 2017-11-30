95913c676d65df51c18ef4833d2787b411854f96
Mason <mason_perry@my.uri.edu>
2017-11-30 11:21:47 -0500

    It Compiles! o^.^o

--- frequencies.cpp
+++ frequencies.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <cstdlib>
 #include <vector>
 #include <fstream>
 
-
 std::string trans(std::string seq) {
   std::string result;
+
 	for (int i = 0; i < (int) seq.length() - 2; i+=3) {
 		std::string tri = seq.substr(i,3);
-
-
+    //tri code 
+  }
+  return result;
+}
 
 std::vector<std::string> newSeq;
 for(unsigned long i = 0; i<= seq.length(); i ++){
@@ -18,11 +21,13 @@ for(unsigned long i = 0; i<= seq.length(); i ++){
   tri += seq[i];
   tri += seq[i+1];
   tri += seq[i+2];
-  std::cout<< tri << endl;
+
+  std::cout << tri << std::endl;
+
   if (tri.length() < 3){
     //returns new sequence if the length of trigram is shorter than three
     return newSeq;
     break;
     }
       //Find the new Sequence
-             else{
+             else {
--- main.cpp
+++ main.cpp
@@ -7,29 +7,25 @@
 using namespace std;
 
 string revcomp(string seq);
-
 int main(int argc, char *argv[]) {
-
-	ifstream infile(argv[1]);
+  ifstream infile(argv[1]);
 	if (!infile.fail()){
     string line;
 		vector<string> myVec;
 		getline(infile,line);
 		std::cout << line << endl;
 
-
     while (getline(infile, line)) {
         // do something with the line
 				myVec.push_back(line);
 		}
-
 		for (int i = (int)myVec.size()-1; i >= 0; i--){
 			std::cout<< revcomp(myVec[i]) << endl;
 		}
     infile.close();
-
-	} else {
-
+  }
+  else {
     cerr << "Could not open file " << argv[1] << endl;
     exit(EXIT_FAILURE);
 	}
+}
