ff82d45a7c772ac8973a330c12ef15825b78f595
Mason <mason_perry@my.uri.edu>
2017-12-04 13:40:06 -0500

    made revisions

--- frequencies.cpp
+++ frequencies.cpp
@@ -8,23 +8,20 @@
 
 int Index(std::string tri){
   int arr[19683] = {0};
+  std::string i;
     arr[i] += 1;
-    return arr;
+  return arr;
 }
 
-std::string trans(std::string seq) {
+std::string trans(std::string seq){
   std::vector<std::string> newseq;
   std::string tri = []
   for(unsigned j = 0; j<= (int) seq.length(); j++){
-    //Makes the trigram
+    //Makes the trigram, while checking frequencies.
     tri += seq[j]-96;
     tri += seq(([j+1]-96)*27);
     tri += seq(([j+2]-96)*27^2);
-    if (tri.length())< 3 {
-      break;
-    }
-    i = Index(tri);
+    if (tri.length())< 3 {break;}
+  return tri;
   }
-
-
 }
