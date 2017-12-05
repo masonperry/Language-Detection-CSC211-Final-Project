ebedc8618a22d8d776b0475344bbc2146cdb29ae
Mason <mason_perry@my.uri.edu>
2017-12-04 20:20:46 -0500

    Progress!!!

--- frequencies.cpp
+++ frequencies.cpp
@@ -7,15 +7,12 @@
 #include <cmath>
 
 using namespace std;
-std::vector<int> Vec(19682);
+//Mikayla Gempp, Mason Perry
 
-void index(int tri, std::vector<int> Vec){
-  Vec[tri] ++;
-}
 //Explain
-void frequencies(std::string seq){
-  //std::vector<std::string> newseq;
+vector<int> frequencies(std::string seq){
   int tri = 0;
+  std::vector<int> miniVec(19682);
   for(unsigned int x = 0; x <= (unsigned int) seq.length()-2; x ++){
     if (seq.length() < 2){
       break;
@@ -32,37 +29,32 @@ void frequencies(std::string seq){
     }
     else{
       tri += ((seq[x] - 96) * (pow)(27,2));
-      std::cout << tri << std::endl;
       tri += ((seq[x+1] - 96) * 27);
-      std::cout << tri << std::endl;
       tri += (seq[x+2] - 96);
-      std::cout << tri<< std::endl;
     }
-    index(tri, Vec);
+
+    miniVec.push_back(tri);
   }
+  return miniVec;
 }
 
-
-<<<<<<< Updated upstream
 int main(int argc, char *argv[]){
   if(argc != 2){
     std::cerr << "Please provide a string to test for trigrams" <<endl;
-    exit(EXIT_FAILURE)
+    exit(EXIT_FAILURE);
   }
-  else{
+  else {
     std::string str = argv[1];
-    frequencies(str);
-    std::cout << Vec << endl;
-    return 0
-  }
-=======
 
-int main(){
-  for(unsigned int x = 0; x <= (unsigned int) Vec.size()-3; x++){
-    std::cout << Vec[x] <<std::endl;
+    std::vector<int> Vec = frequencies(str);
+
+    for(int i = 0; i < 19683; i++){
+      std::string FinalStr = [];
+      to_string(Vec[i]);
+      FinalStr +=
+    }
+    std::cout<<FinalStr<< endl;
+
+    return 0;
   }
-  std::string str = "   ";
-  //std::cout << Vec;
-  return 0;
->>>>>>> Stashed changes
 }
