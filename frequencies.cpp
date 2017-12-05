b10288bb83dd47e72b8c7686c5a88e5550e70a39
Unknown <mikaylag98@my.uri.edu>
2017-12-04 22:57:30 -0500

    Fixed Errors
    Fixeds errors in Frequencies

--- frequencies.cpp
+++ frequencies.cpp
@@ -37,9 +37,7 @@ vector<int> frequencies(std::string seq){
       tri += int ((seq[x+1] - 96) * 27);
       tri += int (seq[x+2] - 96);
     }
-    cout<<"tri: "<<tri<<" ";
     miniVec[tri]+=1;
-    cout<<miniVec[tri];
   }
   //Returns the vector at the end of the Function
   return miniVec;
@@ -54,12 +52,9 @@ int main(int argc, char *argv[]){
     std::string str = argv[1];
     //Makes the vector Vec from the function frequencies
     std::vector<int> Vec = frequencies(str);
-    std::string FinalStr;
     for(int x = 0; x <= (int)Vec.size()-1; x++){
     std::cout << Vec[x] << " ";
   }
-    std::cout<<FinalStr<< endl;
-    std::cout<<Vec.size();
 
     return 0;
   }
